0ceb349e02860c4ae96a5820de5de9dc16416077
mahakhanmk <maha.khan025@gmail.com>
2024-12-15 00:27:00 +0500

    Made one small change

--- learnstreak/settings.py
+++ learnstreak/settings.py
@@ -91,13 +91,14 @@ WSGI_APPLICATION = "learnstreak.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import os
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join('/tmp', 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
